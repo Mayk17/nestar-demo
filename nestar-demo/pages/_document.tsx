c62fbeb34da9d0254fa0b2de52d614e3f1386cde
Mayk17 <tima17@naver.com>
2025-04-10 14:01:14 +0900

    feat: meta tags integrations on document private

--- nestar-demo/pages/_document.tsx
+++ nestar-demo/pages/_document.tsx
@@ -4,7 +4,19 @@ export default function Document() {
   console.log("Document -Pager Router");
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="title" content="Nestar Demo"/>
+        <meta name="robots" content="index,follow" />
+        <link rel="icon" type="image/png" href="/img/logo/favicon.svg" />
+      
+      {/**SEO */}
+      
+        <meta name="keyword" content={"nestar, nestart.uz, devex mern, ,merm nestjs fullstack"} />
+        <meta name="description" content={" Buy and sell properties anywhere anytime in South Korea |" +
+          "한국에서 언제 어디서나 부동산을 사고팔 수 있습니다. |" + "Покупайте и продавайте недвижимость в любом месте и в любое время в Южной Корее"
+        }/>
+        </Head>
       <body>
         <Main />
         <NextScript />

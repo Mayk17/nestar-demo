2658c080ad08b1535b52513a81c8e83b94e46b2a
Mayk17 <tima17@naver.com>
2025-04-14 02:14:34 +0900

    feat: SASS integration

--- nestar-demo/libs/components/layout/LayoutHome.tsx
+++ nestar-demo/libs/components/layout/LayoutHome.tsx
@@ -9,7 +9,7 @@ const withLayoutMain = (Component: any) => {
         <Head>
           <title>Nestar</title>
         </Head>
-        <Stack id="pc-warp">
+        <Stack id="pc-wrap">
           <Stack sx={{ background: "#81c784" }}>Header Home</Stack>
           <Stack id={"main"}>
             <Component {...props} />
--- nestar-demo/pages/_app.tsx
+++ nestar-demo/pages/_app.tsx
@@ -1,20 +1,19 @@
-
 import type { AppProps } from "next/app";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
-import {light } from "../scss/MaterialTheme"
+import { light } from "../scss/MaterialTheme";
 import { useState } from "react";
+import "../scss/app.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   // @ts-ignore
   const [theme, setTheme] = useState(createTheme(light));
-  
+
   // Soket.io redux
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Component {...pageProps} />;
     </ThemeProvider>
-  )
-  
+  );
 }

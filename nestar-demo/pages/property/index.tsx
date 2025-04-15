8b6a4e2e846eb0f5d52f7a48f46ab6fda4eed3bd
Mayk17 <tima17@naver.com>
2025-04-16 06:37:34 +0900

    property page publish 20%

--- nestar-demo/pages/property/index.tsx
+++ nestar-demo/pages/property/index.tsx
@@ -1,12 +1,27 @@
 import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
-import { Container, Stack } from "@mui/material";
+import { Box, Button,  Stack } from "@mui/material";
 import { NextPage } from "next";
+import KeyboardArrowDownRoundedIcon from "@mui/icons-material/KeyboardArrowDownRounded";
+import React, { useState } from "react"; 
 
 const  PropertyList: NextPage= () => {
-
+     const [properties, setProperties] = useState<number[]>([1, 2, 3, 4, 5, 6]);
     return (   
-        <div style= {{ margin:"20px 0"}}>
-            <Stack className="container"> PROPERTY LIST</Stack>
+        <div id={"property-list-page"} style={{position:"relative"}}>
+            <Stack className={"container"}>
+                <Box className={"right"}>
+                    <span>Sort by</span>
+                    <div>
+                        <Button endIcon={<KeyboardArrowDownRoundedIcon />}>New</Button>
+                    </div>
+                    </Box>
+                    <Stack className={"property-page"}>
+                        <Stack className={"filter-config"}>
+                            <Filter />
+                        </Stack>
+
+                    </Stack>
+            </Stack>
         </div>
     )
 }

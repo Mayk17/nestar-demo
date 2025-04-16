f645c7f4f14a90a12f49a839fb56e249572a031b
Mayk17 <tima17@naver.com>
2025-04-17 06:44:44 +0900

    122 dars 50%

--- nestar-demo/pages/property/index.tsx
+++ nestar-demo/pages/property/index.tsx
@@ -1,8 +1,11 @@
 import withLayoutBasic from "@/libs/components/layout/LayoutBasic";
-import { Box, Button,  Stack } from "@mui/material";
+import { Box, Button,  Pagination,  Stack } from "@mui/material";
 import { NextPage } from "next";
 import KeyboardArrowDownRoundedIcon from "@mui/icons-material/KeyboardArrowDownRounded";
 import React, { useState } from "react"; 
+import Filter from "@/libs/components/property/Filter";
+import {Typography} from "@mui/material";
+import PropertyCard from "@/libs/components/property/PropertyCard";
 
 const  PropertyList: NextPage= () => {
      const [properties, setProperties] = useState<number[]>([1, 2, 3, 4, 5, 6]);
@@ -18,6 +21,26 @@ const  PropertyList: NextPage= () => {
                     <Stack className={"property-page"}>
                         <Stack className={"filter-config"}>
                             <Filter />
+                        </Stack>
+                        <Stack className={"main-config"} mb={"76px"}>
+                            <Stack className={"list-config"}>
+                                {properties.map((property, index)=>{
+                                    return <PropertyCard key={index} />;
+                                })}
+                        </Stack>
+                        <Stack className="pagination-config">
+                            <Stack className="paginantion-box">
+                                <Pagination
+                                    page={1}
+                                    count={5}
+                                    shape="circular"
+                                    color="primary"
+                                />
+                            </Stack>
+                            <Stack className="total-result">
+                                <Typography> Total 5 properties available</Typography>
+                            </Stack>
+                            </Stack>
                         </Stack>
 
                     </Stack>

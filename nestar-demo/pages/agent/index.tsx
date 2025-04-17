233067557fc318058a6c2e0aa85ce12f82d89207
Mayk17 <tima17@naver.com>
2025-04-17 15:11:41 +0900

    feat: property list screen 1

--- nestar-demo/pages/agent/index.tsx
+++ nestar-demo/pages/agent/index.tsx
@@ -4,7 +4,6 @@ import { NextPage } from "next";
 import { useState } from "react";
 
 const AgentList: NextPage = () => {
-  const [title, setTitle] = useState<string>("hello");
   return (
     <div style= {{ margin:"20px 0"}}>
             <Stack className="container"> AGENTS LIST</Stack>
